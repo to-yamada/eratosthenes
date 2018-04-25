af9c01237cc9b15c828bb660ff4946b7a87f4cd4
to-yamada <5902143+to-yamada@users.noreply.github.com>
2018-04-26 00:13:00 +0900

    基本の構成のみを作成

---
+++ eratosthenes.rb
@@ -0,0 +1,7 @@
+class Eratosthenes
+  def self.each(upper_bound)
+  end
+end
+
+if $0 == __FILE__
+end
---
+++ spec/eratosthenes_spec.rb
@@ -0,0 +1,9 @@
+require File.expand_path(File.dirname(__FILE__) + '/../eratosthenes')
+
+describe Eratosthenes do
+  describe '.each' do
+    it '与えた引数を超えない素数を返す' do
+    end
+  end
+end
+
