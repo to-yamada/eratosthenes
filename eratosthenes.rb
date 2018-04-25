562a38e70628faa9eab90272ad16c694d0699f3a
to-yamada <5902143+to-yamada@users.noreply.github.com>
2018-04-26 00:58:37 +0900

    実装、テスト完了。最終回答。

--- eratosthenes.rb
+++ eratosthenes.rb
@@ -1,7 +1,29 @@
 class Eratosthenes
   def self.each(upper_bound)
+    prime_list = []
+
+    # 探索リストに2からxまでの整数を昇順で入れる
+    search_list = (2..upper_bound).to_a
+    
+    # 振い落とし操作を探索リストの先頭値がxの平方根に達するまで行う。
+    2.upto(Integer.sqrt(upper_bound)) do |n|
+      # 探索リストの先頭の数を素数リストに移動し、
+      # その倍数を探索リストから篩い落とす。
+      if search_list.include?(n)
+        prime_list << n
+        search_list.delete_if{|x| x % n == 0}
+      end
+    end
+
+    # 探索リストに残った数を素数リストに移動して終了
+    prime_list.concat(search_list)
   end
 end
 
 if $0 == __FILE__
+  if ARGV.size == 1
+    puts Eratosthenes.each(ARGV[0].to_i).join(", ")
+  else
+    puts "#{$0} 整数 の形で実行してください。"
+  end
 end
--- spec/eratosthenes_spec.rb
+++ spec/eratosthenes_spec.rb
@@ -2,7 +2,22 @@ require File.expand_path(File.dirname(__FILE__) + '/../eratosthenes')
 
 describe Eratosthenes do
   describe '.each' do
-    it '与えた引数を超えない素数を返す' do
+    it '与えられた引数までの素数を返す(引数の数自体を含む)' do
+      expect(Eratosthenes.each(5)).to eq [2, 3, 5]
+    end
+
+    it '与えられた引数までの素数を返す(引数の数は含まない)' do
+      expect(Eratosthenes.each(30)).to eq [
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+      ]
+    end
+
+    it '与えられた引数までの素数を返す(Wikipediaの例)' do
+      expect(Eratosthenes.each(120)).to eq [
+          2,   3,   5,   7,  11,  13,  17,  19,  23,  29,
+         31,  37,  41,  43,  47,  53,  59,  61,  67,  71,
+         73,  79,  83,  89,  97, 101, 103, 107, 109, 113
+      ]
     end
   end
 end
